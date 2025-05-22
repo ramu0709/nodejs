82d61990845bd08f65226f0f6458f2c6af7a239a
ramu0709 <168502710+ramu0709@users.noreply.github.com>
2025-05-22 14:00:46 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,6 +1,6 @@
-const sonarqubeScanner = require('sonarqube-scanner');
+const scanner = require('sonarqube-scanner');
 
-sonarqubeScanner(
+scanner(
   {
     serverUrl: 'http://172.21.40.70:9000/',
     options: {
@@ -8,14 +8,9 @@ sonarqubeScanner(
       'sonar.projectName': 'Node JS Application - Sample',
       'sonar.projectDescription': 'This is a Node JS application',
       'sonar.projectVersion': '1.0',
-      'sonar.login': 'squ_e49c6d5fadc72205f815c326baab998ba80932f5',
-      'sonar.language': 'js',
-      'sonar.sourceEncoding': 'UTF-8',
       'sonar.sources': '.',
-      'sonar.javascript.lcov.reportPaths': 'coverage/lcov.info'
+      'sonar.login': 'squ_e49c6d5fadc72205f815c326baab998ba80932f5'
     },
   },
-  () => {
-    console.log('SonarQube scan completed.');
-  }
+  () => process.exit()
 );
