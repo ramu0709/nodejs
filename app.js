c230e53df8d7517b469ef63087b0f2650db536c6
ramu0709 <168502710+ramu0709@users.noreply.github.com>
2025-05-22 14:00:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,38 +1,11 @@
-node
-{
- 
-  stage("CheckOutCodeGit")
-  {
-   git branch: 'master', credentialsId: '65fb834f-a83b-4fe7-8e11-686245c47a65', url: 'https://github.com/bhaskar0504/node-js-application.git'
- }
- 
- stage("Build")
- {
- nodejs(nodeJSInstallationName: 'nodejs15.2.1') {
-        sh 'npm install'
-    }
- }  
- 
-  stage('ExecuteSonarQubeReport') {
-     nodejs(nodeJSInstallationName: 'nodejs15.2.1') {
-        sh 'npm run sonar'
-    }
-      
-        } 
-		
-    stage('UploadintoNexus') {
-       nodejs(nodeJSInstallationName: 'nodejs15.2.1') {
-          sh 'npm publish'
-      }
-      
-          }	
- 
- stage('RunNodeJsApp')
- {
- //sh "./scripts/run.sh"
- nodejs(nodeJSInstallationName: 'nodejs15.2.1') {
-        sh 'npm start &'
-    }
-}    
-    
-}
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Node.js App running on Java 11 compatible environment!');
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
