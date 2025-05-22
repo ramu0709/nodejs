2ccac4a3d25c607e5e7533b732dc05c1864b598e
ramu0709 <168502710+ramu0709@users.noreply.github.com>
2025-05-22 12:50:20 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,17 +1,24 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
-sonarqubeScanner({
-  serverUrl: 'http://172.21.40.70:9000/',
-  options: {
-    'sonar.projectKey': 'NodeJsRamu',
-    'sonar.projectName': 'Node JS Application - Sample',
-    'sonar.projectDescription': 'This is a Node JS application',
-    'sonar.projectVersion': '1.0',
-    'sonar.login': process.env.SONAR_TOKEN,
-    'sonar.language': 'js',
-    'sonar.sourceEncoding': 'UTF-8',
-    'sonar.sources': '.',
+sonarqubeScanner(
+  {
+    serverUrl: 'http://172.21.40.70:9000/',  // Your SonarQube server URL
+    options: {
+      'sonar.projectKey': 'NodeJsRamu',
+      'sonar.projectName': 'Node JS Application - Sample',
+      'sonar.projectDescription': 'This is a Node JS application',
+      'sonar.projectVersion': '1.0',
+      'sonar.login': 'squ_e49c6d5fadc72205f815c326baab998ba80932f5',
+      'sonar.language': 'js',
+      'sonar.sourceEncoding': 'UTF-8',
+      'sonar.sources': '.', // root directory of your Node.js project
+      // Uncomment if you have tests
+      // 'sonar.tests': 'test',
+      // 'sonar.test.inclusions': '**/*.test.js',
+      'sonar.javascript.lcov.reportPaths': 'coverage/lcov.info' // If you generate test coverage
+    },
   },
-}, () => {
-  console.log('SonarQube scan completed.');
-});
+  () => {
+    console.log('SonarQube scan completed.');
+  }
+);
